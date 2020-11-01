fbc02c5266ea778b95153ea2ec9764ecafc061b9
Juana Sucasaire <juana_29_1@hotmail.com>
2020-11-01 13:31:08 -0300

    Creo la carpeta components. Genero el archivo NavBar.js -Una barra de navegacion minimalista-

--- my-e-commerce/src/App.js
+++ my-e-commerce/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Iniciando mi primer proyecto con React!!
-        </p>
-      </header>
+      <NavBar/>
     </div>
   );
 }
---
+++ my-e-commerce/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <nav>
+            <a href="#">Inicio</a>
+            <a href="#">Productos</a>
+            <a href="#">Nosotros</a>
+            <a href="#">Contacto</a>
+        </nav>
+    )
+}
+
+export default NavBar;
